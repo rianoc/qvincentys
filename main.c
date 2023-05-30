ade41b6fde756f907a6ce1306e90f2f51e76e861
David Crossey <dcrossey@kx.com>
2023-05-31 00:42:28 +1000

    Refactored to pass by reference

--- main.c
+++ main.c
@@ -5,24 +5,25 @@
 
 typedef struct geom {
     double sin_sigma, cos_sigma, sigma, sin_alpha, cos_sq_alpha, cos2sigma;
-} Geom;
+} geom; 
 
-Geom geomVar;
+struct geom calcgeo(geom *g, double lam, double u1, double u2) {
 
-struct geom calcGeo(double lam, double u1, double u2) {
+    (*g).sin_sigma = sqrt(pow((cos(u2) * sin(lam)), 2.) + pow(cos(u1)*sin(u2) - sin(u1)*cos(u2)*cos(lam), 2.));
+    (*g).cos_sigma = sin(u1) * sin(u2) + cos(u1) * cos(u2) * cos(lam);
+    (*g).sigma = atan((*g).sin_sigma / (*g).cos_sigma);
+    if ((*g).sigma <= 0) (*g).sigma = M_PI + (*g).sigma;
+    (*g).sin_alpha = (cos(u1) * cos(u2) * sin(lam)) / (*g).sin_sigma;
+    (*g).cos_sq_alpha = 1 - pow((*g).sin_alpha, 2.);
+    (*g).cos2sigma = (*g).cos_sigma - ((2 * sin(u1) * sin(u2)) / (*g).cos_sq_alpha);
 
-    geomVar.sin_sigma = sqrt(pow((cos(u2) * sin(lam)), 2.) + pow(cos(u1)*sin(u2) - sin(u1)*cos(u2)*cos(lam), 2.));
-    geomVar.cos_sigma = sin(u1) * sin(u2) + cos(u1) * cos(u2) * cos(lam);
-    geomVar.sigma = atan(geomVar.sin_sigma / geomVar.cos_sigma);
-    if (geomVar.sigma <= 0) geomVar.sigma = M_PI + geomVar.sigma;
-    geomVar.sin_alpha = (cos(u1) * cos(u2) * sin(lam)) / geomVar.sin_sigma;
-    geomVar.cos_sq_alpha = 1 - pow(geomVar.sin_alpha, 2.);
-    geomVar.cos2sigma = geomVar.cos_sigma - ((2 * sin(u1) * sin(u2)) / geomVar.cos_sq_alpha);
-
-    return(geomVar);
+    return(*g);
 };
 
 K vinc(K latpk, K longpk, K latck, K longck) {
+    
+    geom gvar;
+
     double req = 6378137.0;             //Radius at equator
     double flat = 1 / 298.257223563;    //flattening of earth
     double rpol = (1 - flat) * req;
@@ -51,22 +52,22 @@ K vinc(K latpk, K longpk, K latck, K longck) {
     diff = 1.;
 
     while (fabs(diff) > tol) {
-        geomVar = calcGeo(lam, u1, u2);
-        C = (flat / 16) * geomVar.cos_sq_alpha * (4 + flat * (4 - 3 * geomVar.cos_sq_alpha));
+        calcgeo(&gvar, lam, u1, u2);
+        C = (flat / 16) * gvar.cos_sq_alpha * (4 + flat * (4 - 3 * gvar.cos_sq_alpha));
         lam_pre = lam;
-        lam = lon + (1 - C) * flat * geomVar.sin_alpha * (geomVar.sigma + C * geomVar.sin_sigma * (geomVar.cos2sigma + C * geomVar.cos_sigma * (2 * pow(geomVar.cos2sigma, 2.) - 1)));
+        lam = lon + (1 - C) * flat * gvar.sin_alpha * (gvar.sigma + C * gvar.sin_sigma * (gvar.cos2sigma + C * gvar.cos_sigma * (2 * pow(gvar.cos2sigma, 2.) - 1)));
         diff = fabs(lam_pre - lam);
     }
     
-    geomVar = calcGeo(lam, u1, u2);
+    calcgeo(&gvar, lam, u1, u2);
     
-    usq = geomVar.cos_sq_alpha * ((pow(req, 2.) - pow(rpol, 2.)) / pow(rpol ,2.));
+    usq = gvar.cos_sq_alpha * ((pow(req, 2.) - pow(rpol, 2.)) / pow(rpol ,2.));
     A = 1 + (usq / 16384) * (4096 + usq * (-768 + usq * (320 - 175 * usq)));
     B = (usq / 1024) * (256 + usq * (-128 + usq * (74 - 47 * usq)));
-    delta_sig = B * geomVar.sin_sigma * (geomVar.cos2sigma + 0.25 * B * (geomVar.cos_sigma * (-1 + 2 * pow(geomVar.cos2sigma, 2.)) -
-                                                         (1 / 6) * B * geomVar.cos2sigma * (-3 + 4 * pow(geomVar.sin_sigma, 2.)) *
-                                                         (-3 + 4 * pow(geomVar.cos2sigma, 2.))));
-    dis = rpol * A * (geomVar.sigma - delta_sig);
+    delta_sig = B * gvar.sin_sigma * (gvar.cos2sigma + 0.25 * B * (gvar.cos_sigma * (-1 + 2 * pow(gvar.cos2sigma, 2.)) -
+                                                         (1 / 6) * B * gvar.cos2sigma * (-3 + 4 * pow(gvar.sin_sigma, 2.)) *
+                                                         (-3 + 4 * pow(gvar.cos2sigma, 2.))));
+    dis = rpol * A * (gvar.sigma - delta_sig);
     azi1 = atan2((cos(u2) * sin(lam)), (cos(u1) * sin(u2) - sin(u1) * cos(u2) * cos(lam)));
     
     kF(res)[0]=dis;
